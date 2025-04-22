03bede07c0273106edc505e7fd5a03343182bb57
Didieth <bdidieth@gmail.com>
2025-04-22 16:27:56 -0500

    test: agrega prueba unitaria para el endpoint /nuevo

--- tests/test_main.py
+++ tests/test_main.py
@@ -14,3 +14,11 @@ def test_read_2_root():
     response = client.get("/hello")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello world"}
+    
+#nuevooooooooooooooooooooooo
+
+def test_nuevo_endpoint():
+    response = client.get("/nuevo")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Este es el nuevo endpoint"}
+
