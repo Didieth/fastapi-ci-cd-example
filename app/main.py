43df031a246732d459509e46768dff5bccf6f17f
Didieth <bdidieth@gmail.com>
2025-04-22 16:09:31 -0500

    feat: nuevo endpoint /nuevo

--- app/main.py
+++ app/main.py
@@ -2,12 +2,29 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
     return {"message": "Hello CI/CD"}
 
-
 @app.get("/hello")
 def hello():
     return {"message": "Hello world"}
+
+@app.get("/nuevo")
+def nuevo():
+    return {"message": "Este es el nuevo endpoint"}
+
+
+#from fastapi import FastAPI
+
+#app = FastAPI()
+
+
+#@app.get("/")
+#def read_root():
+ #   return {"message": "Hello CI/CD"}
+
+
+#@app.get("/hello")
+#def hello():
+#    return {"message": "Hello world"}
